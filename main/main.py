a553940b504bb42b8021462e91fd627eee21b5db
subhansaleem <mk.subhansaleem@gmail.com>
2022-04-24 18:16:18 +0500

    structured ver1

--- main/main.py
+++ main/main.py
@@ -6,167 +6,189 @@ import json
 from nltk.tokenize import word_tokenize
 #for lemmatization
 from nltk.stem import WordNetLemmatizer, PorterStemmer
+#for sqrt func
 import math
+#for numpy array
 import numpy as np
-from pprint import pprint
-from collections import Counter
 
 lemmatizer = WordNetLemmatizer()
 ps = PorterStemmer()
 
-f = open("Stopword-List.txt", "r")
-text = f.read()
-stopwords = (word_tokenize(text.lower()))
-vector = set()
-wordDic = []
+#creating stopword list
+def stopwds():
+    f = open("Stopword-List.txt", "r")
+    text = f.read()
+    ls = (word_tokenize(text.lower()))
+    return ls
 
-# looping over all the files
-for i in range(1, 449):
-    # reading from file
-    f = open(f"Abstracts/{i}.txt", "r")
-    text = f.read().lower()
+#for creating index with words, tf, idf, and tf-idf values
 
-    # removing punctuation
-    text = re.sub(r'[^\w\s]', ' ', text)
+    # dic = {
+    #     'term' : {
+    #         'idf' : value,
+    #         'tf': [],
+    #         'tf-idf': []
+    #     }
+    # }
 
-    # tokenization  with nltk
-    words = word_tokenize(text)
+def createIndex():
+    wordDic = []
+    #for all word in 448 documents
+    vector = set()
 
-    # removing stopwords
+    # stopwords = (word_tokenize(text.lower()))
+    stopwords = stopwds()
+
+    # looping over all the files
+    for i in range(1, 449):
+        # reading from file
+        f = open(f"Abstracts/{i}.txt", "r")
+        text = f.read().lower()
+
+        # removing punctuation
+        text = re.sub(r'[^\w\s]', ' ', text)
+
+        # tokenization  with nltk
+        words = word_tokenize(text)
+
+        # removing stopwords
+        words = [word for word in words if word not in stopwords]
+
+        # lemmetization or stemming with nltk.stem - WordNetLemmatizer, PorterStemmer
+
+        for index, items in enumerate(words):
+            #lemmetizating
+            # words[index] = lemmatizer.lemmatize(items)
+
+            #stemming
+            words[index] = ps.stem(items)
+
+        # writing cleaned data to file
+        ff = open(f"Cleaned/{i}.txt", "w")
+        for word in words:
+            ff.write(word + " ")
+
+        #creating word list of documents
+        wordDic.append(words)
+
+        for word in words:
+            vector.add(word)
+
+    vector = list(vector)
+    dic = {}
+
+    for vecIndex, vecItem in enumerate(vector):
+        #df value
+        dfCount = 0
+        tempList = []
+
+        for docIndex, docItem in enumerate(wordDic):
+            count = docItem.count(vecItem)
+            tempList.append(count)
+
+            if count != 0:
+                dfCount += 1
+
+        #creating dic for index
+        dic[vecItem] = {}
+        dic[vecItem]['tf'] = tempList
+        dic[vecItem]['idf'] = math.log2(448/dfCount)
+        dic[vecItem]['tf-idf'] = [ dic[vecItem]['idf'] * element if element != 0 else 0 for element in tempList ]
+
+    #writing dictionary to file
+    json.dump(dic, open("Index.txt", 'w'))
+
+    #writing vector to file
+    json.dump(vector, open("vector.txt",'w'))
+
+#for searching query in documents
+def search(query):
+    alpha = 0.001
+    #loading index file
+    dic = json.load(open("Index.txt"))
+    #loading vector file
+    vector = json.load(open("vector.txt"))
+    stopwords = stopwds()
+    docVector = []
+
+    #creating a vector per doc for calculation
+    for i in range(448):
+        temp2 = []
+        for key, value in sorted(dic.items()):
+            for key2,value2 in value.items():
+                if key2 == 'tf-idf':
+                    temp = value[key2]
+                    temp2.append(temp[i])
+        docVector.append(temp2)
+
+    #converting to numpy array for calculation
+    docVector = np.array(docVector)
+
+    #normalizing vector for optimization
+    for i in range(448):
+        docVector[i]= docVector[i] / math.sqrt(sum(docVector[i]**2))
+
+    #tokening query
+    words = word_tokenize(query)
+    #removing stopwords
     words = [word for word in words if word not in stopwords]
 
-    # lemmetization  with nltk.stem - WordNetLemmatizer
 
-    for index, items in enumerate(words):
-        # words[index] = lemmatizer.lemmatize(items)
-        words[index] = ps.stem(items)
+    for index, item in enumerate(words):
+         item = item.lower()
 
-    # writing cleaned data to file
-    ff = open(f"Cleaned/{i}.txt", "w")
-    for word in words:
-        ff.write(word + " ")
+         #lemmatizing
+         # words[index] = lemmatizer.lemmatize(item)
 
-    wordDic.append(words)
+         #stemming
+         words[index] = ps.stem(item)
 
-    for word in words:
-        vector.add(word)
+    queryDic = {}
 
+    #creating tf,tf-idf values for query
+    for vecIndex, vecItem in enumerate(vector):
+        tempList = []
 
-# pprint(len(wordDic))
-vector = list(vector)
-# print(len(vector))
-
-# dic = {
-#     'term' : {
-#         'idf' : value,
-#         'tf': [],
-#         'tf-idf': []
-#     }
-# }
-
-dic = {}
-
-for vecIndex, vecItem in enumerate(vector):
-    dfCount = 0
-    tempList = []
-
-    for docIndex, docItem in enumerate(wordDic):
-
-        # counter = Counter(docItem)
-        # count = counter[vecItem]
-        # tempList.append(count)
-        count = docItem.count(vecItem)
+        count = words.count(vecItem)
         tempList.append(count)
 
-        if count != 0:
-            dfCount += 1
+        queryDic[vecItem] = {}
+        queryDic[vecItem]['tf'] = count
+        queryDic[vecItem]['tf-idf'] = dic[vecItem]['idf'] * count
 
-    dic[vecItem] = {}
-    dic[vecItem]['tf'] = tempList
-    dic[vecItem]['idf'] = math.log2(448/dfCount)
-    dic[vecItem]['tf-idf'] = [ dic[vecItem]['idf'] * element if element != 0 else 0 for element in tempList ]
-
-#writing dictionary to file
-# json.dump(dic, open("Index.txt", 'w'))
-
-
-docVector = []
-
-
-temp2 = []
-for i in range(448):
-    temp2 = []
-    for key, value in sorted(dic.items()):
-        for key2,value2 in value.items():
+    queryVector = []
+    for key, value in sorted(queryDic.items()):
+        for key2, value2 in value.items():
             if key2 == 'tf-idf':
-                temp = value[key2]
-                temp2.append(temp[i])
-                # print(i,temp[i])
-    docVector.append(temp2)
+               queryVector.append(value2)
 
-docVector = np.array(docVector)
-# print(docVector[0])
+    queryVector = np.array(queryVector)
 
-# v = np.array([2,5,0,3])
-for i in range(448):
-    docVector[i]= docVector[i] / math.sqrt(sum(docVector[i]**2))
-# pprint(dic)
+    #normalizing query for optimization
+    queryVector = queryVector / math.sqrt(sum(queryVector**2))
 
+    #calculating for product of documents and query
+    for i in range(448):
+        ans = sum(x * y for x, y in zip(docVector[i], queryVector))
+        if  ans > alpha:
+            print(i+1)
 
+def main():
+    # for creating index
+    # createIndex()
 
+    while True:
+        print("\n\n1. Search Query\n2. Exit")
+        inp = input()
+        if int(inp) == 1:
+            query = input("Enter Query: ")
+            search(query)
+        else:
+            exit()
 
-query = 'weak heuristic'
-
-words = word_tokenize(query)
-words = [word for word in words if word not in stopwords]
-
-
-for index, item in enumerate(words):
-     item = item.lower()
-     # words[index] = lemmatizer.lemmatize(item)
-     words[index] = ps.stem(item)
-
-queryDic = {}
-
-for vecIndex, vecItem in enumerate(vector):
-    dfCount = 0
-    tempList = []
-
-    count = words.count(vecItem)
-    tempList.append(count)
-
-    queryDic[vecItem] = {}
-    queryDic[vecItem]['tf'] = count
-    queryDic[vecItem]['tf-idf'] = dic[vecItem]['idf'] * count
-
-
-queryVector = []
-for key, value in sorted(queryDic.items()):
-    for key2, value2 in value.items():
-        if key2 == 'tf-idf':
-           queryVector.append(value2)
-
-queryVector = np.array(queryVector)
-queryVector = queryVector / math.sqrt(sum(queryVector**2))
-
-# pprint(len(queryVector))
-# pprint(len(docVector[0]))
-
-
-for i in range(448):
-    if sum(x*y for x,y in zip(docVector[i],queryVector)) > 0.0:
-        print(i+1)
-    # if i == 360:
-    #     print(sum(x*y for x,y in zip(docVector[i],queryVector)))
-
-
-
-
-
-
-
-
-
+# drive code
+if __name__ == "__main__":
+    main()
 
 
 
